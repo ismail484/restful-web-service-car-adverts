dcdac30d0166d2f0984dcf63c8bce15f3be685f5
ismailm <ismailm@oclc.org>
2019-10-27 11:02:29 +0100

    enum for fuel types

---
+++ src/main/java/com/caradverts/carAdverts/CarAdvertsApplication.java
@@ -0,0 +1,14 @@
+package com.caradverts.carAdverts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CarAdvertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAdvertsApplication.class, args);
+	}
+
+}
