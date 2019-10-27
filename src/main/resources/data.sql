60ce47fe6f0475ed2d43c4b997d66f96030e8d7e
ismailm <ismailm@oclc.org>
2019-10-27 11:01:57 +0100

    insert initial data into database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Car (id, first_registration, fuel, new_car, price, title, mileage) VALUES (1, To_DATE('01-01-2000', 'dd/mm/yyyy'), 'GASOLINE', false, '50000$', 'BMW', 50000);
+INSERT INTO Car (id, fuel, new_car, price, title, mileage) VALUES (2, 'GASOLINE', true, '50000$', 'BMW', 0);
